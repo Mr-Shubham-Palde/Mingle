c8dccee026d3e3ef593bb84861d8bbe9d2c8546c
Mr-Shubham-Palde <shubhampalde96@gmail.com>
2025-10-02 21:47:08 +0530

    part 7 done

--- src/app.js
+++ src/app.js
@@ -3,26 +3,80 @@ const connectDB = require("./config/database")
 
 const User = require("./models/user")
 
-const app =express();
+const app = express();
+app.use(express.json())
 
-app.post("/signup",async(req,res)=>{
-    const userobj = {
-        firstName:"Virat",
-        lastName:"Palde",
-        emailid:"virat@gmail.com",
-        password:"virat@123",
-    }
-    //creating a new instance of the model
-    const user = new User(userobj)
+app.post("/signup", async (req, res) => {
+
     try {
+        console.log(req.body)
+        const data = req.body;
+
+        const user = new User(data)
         await user.save()
-        res.send("User added successfully")
+        res.send("User added to database successfully")
+
     } catch (error) {
-        res.status(400).send("Error while adding the user")
-        
+        res.status(500).send("Error in saving the user")
     }
 
+})
 
+app.get("/getalldata",async(req,res)=>{
+    try {
+        const alldata = await User.find()
+        res.send(alldata)
+        
+    } catch (error) {
+        res.status(500).send("Error in fetching the data")
+    }
+})
+
+// get the user by emailid
+
+app.get("/user",async(req,res)=>{
+    const useremail = req.body.emailid;
+
+    try {
+        const user = await User.findOne({emailid:useremail})
+        if (user.length===0){
+            return res.status(404).send("User not found")
+        }else{
+            res.send(user)
+        }
+        
+    } catch (error) {
+        res.status(500).send("Error in accessing  the user")
+
+    }
+})
+
+//delete the user by the id 
+app.delete("/deleteuser",async(req,res)=>{
+    try {
+        const userid = req.body.userid
+        
+        const user = await User.findByIdAndDelete(userid)
+        res.send("User deleted successfully")
+    } catch (error) {
+        res.status(500).send("Error in deleting the user")
+    }
+})
+
+
+//update the user
+
+app.patch("/updateuser",async(req,res)=>{
+    try {
+        const data = req.body;
+        const userid = req.body.userid
+
+        await User.findByIdAndUpdate({_id:userid},data)
+        res.send("User updated successfully")
+        
+    } catch (error) {
+        res.status(500).send("Error in updating the user")
+    }
 })
 
 
@@ -30,14 +84,15 @@ app.post("/signup",async(req,res)=>{
 
 
 
-connectDB().then(()=>{
+
+connectDB().then(() => {
     console.log("Connected to DB successfully")
 
-    app.listen(3000,()=>{
+    app.listen(3000, () => {
         console.log("Server is listening at port 3000")
     })
-    
+
 })
-.catch((err) => {
-    console.error("Database cannot be connected")
-})
+    .catch((err) => {
+        console.error("Database cannot be connected")
+    })
--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
         type:String,
 
     },
-    email:{
+    emailid:{
         type:String,
         
     },
